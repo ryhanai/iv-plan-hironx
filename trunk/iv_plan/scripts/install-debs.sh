1f34a7564df60e5169b8d683af4c07269c0bbfa5
ryhanai <ryhanai@91089df6-1737-e318-ccc2-348ea22bffda>
2012-05-31 07:41:19 +0000

    added some packages to install-debs.sh

--- trunk/iv_plan/scripts/install-debs.sh
+++ trunk/iv_plan/scripts/install-debs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get install ipython python-numpy python-scipy python-ply libboost-python-dev \
-    libboost-signals1.40.0 libgtkglextmm-x11-1.2-0 libglademm-2.4-dev
+    libboost-signals1.40.0 libgtkglextmm-x11-1.2-0 libglademm-2.4-dev liblapack-dev liblapack3gf libboost-thread1.40.0
